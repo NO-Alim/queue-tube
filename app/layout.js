7ead048f561e0a3aca55967a8567c4d0c455c179
unknown <abdulalimrakib53@gmail.com>
2024-12-30 19:46:05 +0600

    og image path change

--- app/layout.js
+++ app/layout.js
@@ -21,7 +21,7 @@ export const metadata = {
     "Queue Tube lets you organize, watch, and take notes on your favorite video playlists distraction-free. Add up to 20 playlists, set favorites, and track videos smarter.",
 
   openGraph: {
-    images: ["https://queue-tube.vercel.app/assets/thumbnail.jpg"],
+    images: ["/thumbnail.jpg"],
   },
 };
 
